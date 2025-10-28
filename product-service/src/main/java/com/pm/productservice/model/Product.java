2920f07237ec4eb43f57754affc44df1215b71f3
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-10-28 10:57:03 +0530

    feat(product): add product module with DB configuration

---
+++ product-service/src/main/java/com/pm/productservice/model/Product.java
@@ -0,0 +1,46 @@
+package com.pm.productservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String category;
+    private String name;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private ProductStatus status;
+
+    private String quantity;
+    private Double price;
+    private boolean isForSale;
+    private LocalDate expiryDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
---
+++ product-service/src/main/java/com/pm/productservice/model/ProductStatus.java
@@ -0,0 +1,7 @@
+package com.pm.productservice.model;
+
+public enum ProductStatus {
+    IN_STOCK,
+    LOW_STOCK,
+    OUT_OF_STOCK,
+}
