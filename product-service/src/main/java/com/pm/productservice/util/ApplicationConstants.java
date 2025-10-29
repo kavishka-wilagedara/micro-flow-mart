0a1224e13a5cea440bce604dc2b8c4d5e8ff312d
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-10-29 16:28:13 +0530

    feat(util): add ApplicationConstants class to reuse error message

---
+++ product-service/src/main/java/com/pm/productservice/util/ApplicationConstants.java
@@ -0,0 +1,10 @@
+package com.pm.productservice.util;
+
+public final class ApplicationConstants {
+
+    private ApplicationConstants() {
+        throw new UnsupportedOperationException("This class can't be instantiated");
+    }
+
+    public static final String PRODUCT_NOT_FOUND_MESSAGE = "Product not found with id:";
+}
