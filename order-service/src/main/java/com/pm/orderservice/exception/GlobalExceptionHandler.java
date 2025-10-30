43cb949272b529ddb2c12abc3bf0d5eec72e602e
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-10-31 01:17:36 +0530

    add GlobalExceptionHandler

---
+++ order-service/src/main/java/com/pm/orderservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.pm.orderservice.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    public static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidateException(
+            MethodArgumentNotValidException ex) {
+
+        log.error("Handling global exception", ex);
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(
+                error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNotFoundException(
+            NotFoundException ex){
+
+        log.error("Handling not found exception", ex);
+
+        Map<String, String> errors = new HashMap<>();
+
+        errors.put("message", "Product not found");
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(CustomBusinessException.class)
+    public ResponseEntity<Map<String, String>> handleCustomBusinessException(
+            NotFoundException ex){
+
+        log.error("Handling custom business exception", ex);
+
+        Map<String, String> errors = new HashMap<>();
+
+        errors.put("message", "Order creation failed");
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
