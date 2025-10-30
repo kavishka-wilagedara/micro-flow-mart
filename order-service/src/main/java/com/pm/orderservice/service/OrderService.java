03699a995e970a53acc1442497ff42192fd8a9a2
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-10-30 15:23:55 +0530

    feat(order): define order-services

---
+++ order-service/src/main/java/com/pm/orderservice/service/OrderService.java
@@ -0,0 +1,15 @@
+package com.pm.orderservice.service;
+
+import com.pm.orderservice.dto.request.OrderRequest;
+import com.pm.orderservice.dto.response.OrderResponse;
+
+import java.util.List;
+
+public interface OrderService {
+
+    OrderResponse createOrder(OrderRequest orderRequest);
+    OrderResponse updateOrder(OrderRequest orderRequest);
+    void deleteOrder(long orderId);
+    OrderResponse getOrder(long orderId);
+    List<OrderResponse> getAllOrders();
+}
