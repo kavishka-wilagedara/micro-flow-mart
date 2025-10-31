969aa86a3b4ce7bdf53b6979d664a445b3c2a331
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-10-31 18:31:39 +0530

    add validations for OrderRequest

--- order-service/src/main/java/com/pm/orderservice/dto/request/OrderRequest.java
+++ order-service/src/main/java/com/pm/orderservice/dto/request/OrderRequest.java
@@ -1,11 +1,19 @@
 package com.pm.orderservice.dto.request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class OrderRequest {
 
+    @Valid
+    @NotNull(message = "Receiver details are required")
     private RecieverDetailsRequest recieverDetailsRequest;
+    @Valid
+    @NotNull(message = "Receiver address are required")
     private RecieverAddressRequest recieverAddressRequest;
+    @Valid
+    @NotNull(message = "Product details are required")
     private ProductDetailsRequest productDetailsRequest;
 }
