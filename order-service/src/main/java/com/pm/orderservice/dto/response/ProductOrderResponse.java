0f5e4196e335f3dcdd8c7cfbfabb938824977d89
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-10-31 18:25:04 +0530

    add ProductOrderResponse.java for product-service response

--- order-service/src/main/java/com/pm/orderservice/dto/response/ProductOrderResponse.java
+++ order-service/src/main/java/com/pm/orderservice/dto/response/ProductOrderResponse.java
@@ -1,17 +1,29 @@
 package com.pm.orderservice.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
-public class ProductOrderResponse{
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductOrderResponse {
 
-    private String category;
-    private String name;
-    private String description;
-    private String status;
-    private Double price;
-    private boolean isForSale;
-    private LocalDate expiryDate;
+        private Long id;
+        private String category;
+        private String name;
+        private String description;
+        private String status;
+        private int quantity;
+        private Double price;
+        private boolean isForSale;
+        private LocalDate expiryDate;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+
 }
