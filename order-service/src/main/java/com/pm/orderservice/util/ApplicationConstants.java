951d89002a9a87315a0859cf93a65e823e8dd39e
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-11-06 13:50:32 +0530

    add util class for reuse constants

---
+++ order-service/src/main/java/com/pm/orderservice/util/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package com.pm.orderservice.util;
+
+public class ApplicationConstants {
+
+    private ApplicationConstants() { throw new UnsupportedOperationException("This class can't be instantiated"); }
+
+    public static final String ORDER_NOT_FOUND_MESSAGE = "Order not found with id: ";
+}
