2565f793d2d5bc40c406cc08a3cbd48d221dc375
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-11-07 14:33:08 +0530

    define request and response dto with validations

--- inventory-service/src/main/java/com/pm/inventoryservice/dto/request/InventoryRequest.java
+++ inventory-service/src/main/java/com/pm/inventoryservice/dto/request/InventoryRequest.java
@@ -1,4 +1,23 @@
 package com.pm.inventoryservice.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class InventoryRequest {
+
+    @NotNull(message = "Order ID required")
+    private long orderId;
+    @NotNull(message = "Product ID required")
+    private long productId;
+    @NotNull(message = "Product name required")
+    private String productName;
+    @NotNull(message = "Order quantity required")
+    @Min(value = 1, message = "Quantity must be at least 1")
+    private int quantity;
+    @NotNull(message = "Unit price required")
+    private double unitPrice;
+    @NotNull(message = "Total price required")
+    private double totalPrice;
 }
--- inventory-service/src/main/java/com/pm/inventoryservice/dto/response/InventoryResponse.java
+++ inventory-service/src/main/java/com/pm/inventoryservice/dto/response/InventoryResponse.java
@@ -1,4 +1,21 @@
 package com.pm.inventoryservice.dto.response;
 
+import com.pm.inventoryservice.model.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class InventoryResponse {
+
+    private long id;
+    private long orderId;
+    private long productId;
+    private String productName;
+    private int quantity;
+    private double unitPrice;
+    private double totalPrice;
+    private OrderStatus orderStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
