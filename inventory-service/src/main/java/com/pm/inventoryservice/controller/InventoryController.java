c84099c8426258d96778cbded5b388094aee5e9b
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-11-10 19:09:17 +0530

    add inventory delete endpoint

--- inventory-service/src/main/java/com/pm/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/pm/inventoryservice/controller/InventoryController.java
@@ -1,4 +1,27 @@
 package com.pm.inventoryservice.controller;
 
+import com.pm.inventoryservice.service.InventoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/inventory")
 public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @DeleteMapping("/delete/{inventoryId}")
+    public ResponseEntity<String> deleteInventory(@PathVariable("inventoryId") long inventoryId) {
+
+        inventoryService.deleteInventory(inventoryId);
+
+        return ResponseEntity.ok().body("Deleted inventory");
+    }
 }
