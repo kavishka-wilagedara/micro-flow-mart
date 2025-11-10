9474d3ee2d9740abe85bad329a37664b6db22f89
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-11-10 16:39:05 +0530

    add InventoryCreated feature via consumer

--- inventory-service/src/main/java/com/pm/inventoryservice/service/impl/InventoryServiceImpl.java
+++ inventory-service/src/main/java/com/pm/inventoryservice/service/impl/InventoryServiceImpl.java
@@ -2,14 +2,43 @@ package com.pm.inventoryservice.service.impl;
 
 import com.pm.inventoryservice.dto.request.InventoryRequest;
 import com.pm.inventoryservice.dto.response.InventoryResponse;
+import com.pm.inventoryservice.mapper.InventoryMapper;
+import com.pm.inventoryservice.model.Inventory;
+import com.pm.inventoryservice.model.OrderStatus;
+import com.pm.inventoryservice.repo.InventoryRepository;
 import com.pm.inventoryservice.service.InventoryService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class InventoryServiceImpl implements InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+    private final InventoryMapper inventoryMapper;
+
+    public InventoryServiceImpl(
+            InventoryRepository inventoryRepository,
+            InventoryMapper inventoryMapper
+    ) {
+        this.inventoryRepository = inventoryRepository;
+        this.inventoryMapper = inventoryMapper;
+    }
+
     @Override
     public InventoryResponse createInventory(InventoryRequest inventoryRequest) {
-        return null;
+
+        Inventory newInventory = new Inventory();
+
+        newInventory.setOrderId(inventoryRequest.getOrderId());
+        newInventory.setProductId(inventoryRequest.getProductId());
+        newInventory.setProductName(inventoryRequest.getProductName());
+        newInventory.setOrderStatus(OrderStatus.ORDER_CREATED);
+
+        inventoryRepository.save(newInventory);
+
+        return inventoryMapper.toInventoryResponse(newInventory);
+
     }
 
     @Override
