eb13223088c0155c8f78b28fd72bb1f6e2fdf0a5
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-11-10 18:44:13 +0530

    add newTopic for orderDelete

--- order-service/src/main/java/com/pm/orderservice/config/KafkaTopicConfig.java
+++ order-service/src/main/java/com/pm/orderservice/config/KafkaTopicConfig.java
@@ -15,6 +15,9 @@ public class KafkaTopicConfig {
     @Value("${kafka.topic.order.updated}")
     private String orderUpdatedTopic;
 
+    @Value("${kafka.topic.order.deleted}")
+    private String orderDeletedTopic;
+
     @Bean
     public NewTopic orderCreatedTopic() {
         return TopicBuilder
@@ -28,6 +31,15 @@ public class KafkaTopicConfig {
     public NewTopic orderUpdatedTopic() {
         return TopicBuilder
                 .name(orderUpdatedTopic)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic orderDeletedTopic() {
+        return TopicBuilder
+                .name(orderDeletedTopic)
                 .partitions(1)
                 .replicas(1)
                 .build();
