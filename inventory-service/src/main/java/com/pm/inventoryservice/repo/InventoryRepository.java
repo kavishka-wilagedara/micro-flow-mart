bd44215bcd6ae1d873df152da00f45c191476d4c
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-11-10 18:21:17 +0530

    add findInventoryByOrderId method

--- inventory-service/src/main/java/com/pm/inventoryservice/repo/InventoryRepository.java
+++ inventory-service/src/main/java/com/pm/inventoryservice/repo/InventoryRepository.java
@@ -3,5 +3,9 @@ package com.pm.inventoryservice.repo;
 import com.pm.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    Optional<Inventory> findByOrderId(Long orderId);
 }
