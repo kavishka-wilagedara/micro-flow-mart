8d0170fb9a3e19b043656e66de4946d178307c7d
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-11-10 19:19:11 +0530

    add getInventoryBy id endpoint

--- inventory-service/src/main/java/com/pm/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/pm/inventoryservice/controller/InventoryController.java
@@ -1,11 +1,9 @@
 package com.pm.inventoryservice.controller;
 
+import com.pm.inventoryservice.dto.response.InventoryResponse;
 import com.pm.inventoryservice.service.InventoryService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/inventory")
@@ -23,5 +21,13 @@ public class InventoryController {
         inventoryService.deleteInventory(inventoryId);
 
         return ResponseEntity.ok().body("Deleted inventory");
+    }
+
+    @GetMapping("/get/{inventoryId}")
+    public ResponseEntity<InventoryResponse> getInventory(@PathVariable("inventoryId") long inventoryId) {
+
+        InventoryResponse inventoryResponse = inventoryService.getInventory(inventoryId);
+
+        return ResponseEntity.ok().body(inventoryResponse);
     }
 }
